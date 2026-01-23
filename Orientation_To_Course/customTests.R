fd737507408c35a8c20b1278ba8259a99c59efe9
ehangen <emilyjhangen@gmail.com>
2026-01-22 19:13:52 -0500

    Update customTests.R
    Remove submit log

--- Orientation_To_Course/customTests.R
+++ Orientation_To_Course/customTests.R
@@ -57,33 +57,3 @@ go_to_brightspace_if_yes <- function() {
   }
 }
 
-submit_log <- function(){
-  selection <- getState()$val
-  if(selection == "Yes"){
-    # Please edit the link below
-    pre_fill_link <- "https://docs.google.com/forms/d/e/1FAIpQLSfqF-YfYrBjTbM9eVG_Ygbv6VpWVzgA1gyEW-NXyzX9wJ5U4A/viewform?usp=pp_url&entry.996111921"
-    
-    # Do not edit the code below
-    if(!grepl("=$", pre_fill_link)){
-      pre_fill_link <- paste0(pre_fill_link, "=")
-    }
-    
-    p <- function(x, p, f, l = length(x)){if(l < p){x <- c(x, rep(f, p - l))};x}
-    
-    temp <- tempfile()
-    log_ <- getLog()
-    nrow_ <- max(unlist(lapply(log_, length)))
-    log_tbl <- data.frame(user = rep(log_$user, nrow_),
-                          course_name = rep(log_$course_name, nrow_),
-                          lesson_name = rep(log_$lesson_name, nrow_),
-                          question_number = p(log_$question_number, nrow_, NA),
-                          correct = p(log_$correct, nrow_, NA),
-                          attempt = p(log_$attempt, nrow_, NA),
-                          skipped = p(log_$skipped, nrow_, NA),
-                          datetime = p(log_$datetime, nrow_, NA),
-                          stringsAsFactors = FALSE)
-    write.csv(log_tbl, file = temp, row.names = FALSE)
-    encoded_log <- base64encode(temp)
-    browseURL(paste0(pre_fill_link, encoded_log))
-  }
-}
